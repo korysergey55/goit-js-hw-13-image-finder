8c0759a6ecf792140be6aff11c0d6f72e99c71a2
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-20 23:28:38 +0300

    css

--- src/js/api.js
+++ src/js/api.js
@@ -2,9 +2,10 @@ const keyPixabay = '21698474-fb36d7b3400c91ab3d227d6db';
 
 const fetchData = (data, page = 1) => {
   const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${data}&page=${page}&per_page=12&key=${keyPixabay}`;
+
   return fetch(BASE_URL)
     .then(response => response.json())
-    .catch(err => console.log(err));
+    .catch(err => alert(err));
 };
 
 export { fetchData };
