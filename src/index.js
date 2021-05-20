d38fa628b0cd264ff285ed924e2fa5d0f5bc42fc
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-20 17:54:19 +0300

    hbs-done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // import './sass/main.scss';
+import { fetchData } from './js/api';
+import cart_galary from './tpl/cart_galary.hbs';
+
+const formRef = document.querySelector('.search-form');
+const inputRef = document.querySelector('.intup');
+const galleryRef = document.querySelector('.gallery');
+const inputBtnRef = document.querySelector('.input-button');
+
+formRef.addEventListener('submit', sentForm);
+
+function sentForm(event) {
+  event.preventDefault();
+  const value = inputRef.value;
+  if (value == '') return;
+
+  fetchData(value).then(data => {
+    console.log(data);
+    const carts = cart_galary(data.hits);
+    galleryRef.insertAdjacentHTML('beforeend', carts);
+  });
+}
