a0e1abcded562872d3c3e3f273d07592ad57c280
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-21 16:45:21 +0300

    hw-done

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -126,9 +126,9 @@ Object.defineProperty(exports, "__esModule", {
 exports.fetchData = void 0;
 const keyPixabay = '21698474-fb36d7b3400c91ab3d227d6db';
 
-const fetchData = (data, page = 1) => {
-  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${data}&page=${page}&per_page=12&key=${keyPixabay}`;
-  return fetch(BASE_URL).then(response => response.json()).catch(err => console.log(err));
+const fetchData = (searchWord, page) => {
+  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchWord}&page=${page}&per_page=12&key=${keyPixabay}`;
+  return fetch(BASE_URL).then(response => response.json()).catch(err => alert(err));
 };
 
 exports.fetchData = fetchData;
@@ -2174,7 +2174,7 @@ const templateFunction = _handlebars.default.template({
       return undefined;
     };
 
-    return "  <li class='photo-card'>\r\n    <img data-big-imege='" + alias4((helper = (helper = lookupProperty(helpers, "largeImageURL") || (depth0 != null ? lookupProperty(depth0, "largeImageURL") : depth0)) != null ? helper : alias2, typeof helper === alias3 ? helper.call(alias1, {
+    return "  <li class='photo-card'>\r\n    <img data-big-image='" + alias4((helper = (helper = lookupProperty(helpers, "largeImageURL") || (depth0 != null ? lookupProperty(depth0, "largeImageURL") : depth0)) != null ? helper : alias2, typeof helper === alias3 ? helper.call(alias1, {
       "name": "largeImageURL",
       "hash": {},
       "data": data,
@@ -2272,7 +2272,7 @@ const templateFunction = _handlebars.default.template({
           "column": 21
         }
       }
-    }) : helper)) + "\r\n      </p>\r\n    </div>\r\n  </li>\r\n";
+    }) : helper)) + "\r\n      </p>\r\n    </div>\r\n  </li>\r\n\r\n";
   },
   "compiler": [8, ">= 4.3.0"],
   "main": function (container, depth0, helpers, partials, data) {
@@ -2297,7 +2297,7 @@ const templateFunction = _handlebars.default.template({
           "column": 0
         },
         "end": {
-          "line": 24,
+          "line": 25,
           "column": 9
         }
       }
@@ -2308,12 +2308,97 @@ const templateFunction = _handlebars.default.template({
 
 var _default = templateFunction;
 exports.default = _default;
-},{"handlebars/dist/handlebars.runtime":"../node_modules/handlebars/dist/handlebars.runtime.js"}],"index.js":[function(require,module,exports) {
+},{"handlebars/dist/handlebars.runtime":"../node_modules/handlebars/dist/handlebars.runtime.js"}],"../node_modules/basiclightbox/dist/basicLightbox.min.js":[function(require,module,exports) {
+var define;
+var global = arguments[3];
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{("undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:this).basicLightbox=e()}}((function(){return function e(n,t,o){function r(c,u){if(!t[c]){if(!n[c]){var s="function"==typeof require&&require;if(!u&&s)return s(c,!0);if(i)return i(c,!0);var a=new Error("Cannot find module '"+c+"'");throw a.code="MODULE_NOT_FOUND",a}var l=t[c]={exports:{}};n[c][0].call(l.exports,(function(e){return r(n[c][1][e]||e)}),l,l.exports,e,n,t,o)}return t[c].exports}for(var i="function"==typeof require&&require,c=0;c<o.length;c++)r(o[c]);return r}({1:[function(e,n,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.create=t.visible=void 0;var o=function(e){var n=arguments.length>1&&void 0!==arguments[1]&&arguments[1],t=document.createElement("div");return t.innerHTML=e.trim(),!0===n?t.children:t.firstChild},r=function(e,n){var t=e.children;return 1===t.length&&t[0].tagName===n},i=function(e){return null!=(e=e||document.querySelector(".basicLightbox"))&&!0===e.ownerDocument.body.contains(e)};t.visible=i;t.create=function(e,n){var t=function(e,n){var t=o('\n\t\t<div class="basicLightbox '.concat(n.className,'">\n\t\t\t<div class="basicLightbox__placeholder" role="dialog"></div>\n\t\t</div>\n\t')),i=t.querySelector(".basicLightbox__placeholder");e.forEach((function(e){return i.appendChild(e)}));var c=r(i,"IMG"),u=r(i,"VIDEO"),s=r(i,"IFRAME");return!0===c&&t.classList.add("basicLightbox--img"),!0===u&&t.classList.add("basicLightbox--video"),!0===s&&t.classList.add("basicLightbox--iframe"),t}(e=function(e){var n="string"==typeof e,t=e instanceof HTMLElement==1;if(!1===n&&!1===t)throw new Error("Content must be a DOM element/node or string");return!0===n?Array.from(o(e,!0)):"TEMPLATE"===e.tagName?[e.content.cloneNode(!0)]:Array.from(e.children)}(e),n=function(){var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{};if(null==(e=Object.assign({},e)).closable&&(e.closable=!0),null==e.className&&(e.className=""),null==e.onShow&&(e.onShow=function(){}),null==e.onClose&&(e.onClose=function(){}),"boolean"!=typeof e.closable)throw new Error("Property `closable` must be a boolean");if("string"!=typeof e.className)throw new Error("Property `className` must be a string");if("function"!=typeof e.onShow)throw new Error("Property `onShow` must be a function");if("function"!=typeof e.onClose)throw new Error("Property `onClose` must be a function");return e}(n)),c=function(e){return!1!==n.onClose(u)&&function(e,n){return e.classList.remove("basicLightbox--visible"),setTimeout((function(){return!1===i(e)||e.parentElement.removeChild(e),n()}),410),!0}(t,(function(){if("function"==typeof e)return e(u)}))};!0===n.closable&&t.addEventListener("click",(function(e){e.target===t&&c()}));var u={element:function(){return t},visible:function(){return i(t)},show:function(e){return!1!==n.onShow(u)&&function(e,n){return document.body.appendChild(e),setTimeout((function(){requestAnimationFrame((function(){return e.classList.add("basicLightbox--visible"),n()}))}),10),!0}(t,(function(){if("function"==typeof e)return e(u)}))},close:c};return u}},{}]},{},[1])(1)}));
+},{}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/basiclightbox/dist/basicLightbox.min.css":[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _api = require("./js/api");
 
 var _cart_galary = _interopRequireDefault(require("./tpl/cart_galary.hbs"));
+
+var basicLightbox = _interopRequireWildcard(require("basiclightbox"));
+
+require("basiclightbox/dist/basicLightbox.min.css");
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -2321,20 +2406,64 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const formRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.intup');
 const galleryRef = document.querySelector('.gallery');
-const inputBtnRef = document.querySelector('.input-button');
+const btnLoadMoreRef = document.querySelector('.load-more');
 formRef.addEventListener('submit', sentForm);
+btnLoadMoreRef.addEventListener('click', loadMore);
+galleryRef.addEventListener('click', biggestFoto);
+let searchWord = '';
+let page = 1;
 
 function sentForm(event) {
   event.preventDefault();
-  const value = inputRef.value;
-  if (value == '') return;
-  (0, _api.fetchData)(value).then(data => {
-    console.log(data);
+  galleryRef.innerHTML = '';
+  page = 1;
+  searchWord = inputRef.value;
+  if (searchWord === '') return;
+  getImages(false);
+}
+
+function loadMore(event) {
+  page += 1;
+  getImages(true);
+}
+
+function getImages(shouldScroll) {
+  (0, _api.fetchData)(searchWord, page).then(data => {
     const carts = (0, _cart_galary.default)(data.hits);
+
+    if (data.hits.length === 0) {
+      alert('Enter correct name of search!!!');
+    }
+
     galleryRef.insertAdjacentHTML('beforeend', carts);
+    btnLoadMoreRef.classList.add('is-open');
+
+    if (data.hits.length < 12) {
+      btnLoadMoreRef.classList.remove('is-open');
+    }
+
+    if (shouldScroll) {
+      scroll();
+    }
   });
 }
-},{"./js/api":"js/api.js","./tpl/cart_galary.hbs":"tpl/cart_galary.hbs"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+function scroll() {
+  window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: 'smooth'
+  });
+}
+
+function biggestFoto(event) {
+  if (event.target.tagName !== 'IMG') return;
+  const largeImageURL = event.target.dataset.bigImage;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+`);
+  instance.show();
+}
+},{"./js/api":"js/api.js","./tpl/cart_galary.hbs":"tpl/cart_galary.hbs","basiclightbox":"../node_modules/basiclightbox/dist/basicLightbox.min.js","basiclightbox/dist/basicLightbox.min.css":"../node_modules/basiclightbox/dist/basicLightbox.min.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -2362,7 +2491,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57126" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55996" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
