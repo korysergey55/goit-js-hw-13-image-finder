b3c24960b32a23b40ed05b445d198e20a810f1b4
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-21 16:28:01 +0300

    hw-done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,67 @@
 // import './sass/main.scss';
 import { fetchData } from './js/api';
 import cart_galary from './tpl/cart_galary.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
 
 const formRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.intup');
 const galleryRef = document.querySelector('.gallery');
-const inputBtnRef = document.querySelector('.input-button');
+const btnLoadMoreRef = document.querySelector('.load-more');
 
 formRef.addEventListener('submit', sentForm);
+btnLoadMoreRef.addEventListener('click', loadMore);
+galleryRef.addEventListener('click', biggestFoto);
+
+let searchWord = '';
+let page = 1;
 
 function sentForm(event) {
   event.preventDefault();
-  const value = inputRef.value;
-  if (value == '') return;
+  galleryRef.innerHTML = '';
+  page = 1;
+  searchWord = inputRef.value;
+  if (searchWord === '') return;
 
-  fetchData(value).then(data => {
-    console.log(data);
+  getImages(false);
+}
+
+function loadMore(event) {
+  page += 1;
+  getImages(true);
+}
+
+function getImages(shouldScroll) {
+  fetchData(searchWord, page).then(data => {
     const carts = cart_galary(data.hits);
+    if(data.hits.length === 0){
+      alert('Enter correct name of search!!!')
+    }
     galleryRef.insertAdjacentHTML('beforeend', carts);
+    btnLoadMoreRef.classList.add('is-open');
+    if (data.hits.length < 12) {
+      btnLoadMoreRef.classList.remove('is-open');
+    }
+    if (shouldScroll) {
+      scroll();
+    }
   });
+}
+
+function scroll() {
+  window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: 'smooth',
+  });
+}
+
+function biggestFoto(event) {
+  if (event.target.tagName !== 'IMG') return;
+
+  const largeImageURL = event.target.dataset.bigImage;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+`);
+  instance.show();
 }
--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 const keyPixabay = '21698474-fb36d7b3400c91ab3d227d6db';
 
-const fetchData = (data, page = 1) => {
-  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${data}&page=${page}&per_page=12&key=${keyPixabay}`;
+const fetchData = (searchWord, page) => {
+  const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchWord}&page=${page}&per_page=12&key=${keyPixabay}`;
 
   return fetch(BASE_URL)
     .then(response => response.json())
